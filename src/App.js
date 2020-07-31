1ee81fbb06d49cd752c98e227e2e03fc1c7c2556
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 13:53:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div className="App">
+      <Palette />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Palette extends Component {
+
+    render() {
+
+        return(
+            <div>
+                <h1>Palette js</h1>
+            </div>
+        )
+    }
+
+}
+
+
+export default Palette;
